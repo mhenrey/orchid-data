3434b6599b40ad2da231211f7910ca49d9601a64
Mike Henrey <mike.henrey@gmail.com>
2016-09-20 21:43:20 -0700

    initial checkin

---
+++ OrchidHisser/src/MapSettings.java
@@ -0,0 +1,13 @@
+import java.io.File;
+
+public class MapSettings {
+
+	SchoolSettings schoolSettings;
+	
+	
+	public static MapSettings LoadMapSettings(File file) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
---
+++ OrchidHisser/src/OrchidHisser.java
@@ -0,0 +1,38 @@
+import java.io.File;
+
+public class OrchidHisser {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello world");
+
+		File file = new File("Settings.xml");
+
+		MapSettings settings = MapSettings.LoadMapSettings(file);
+		DownloadMapSources();
+		ConcatenateMapSources();
+		MakeTiles();
+		CorrelateScoresWithSchools();
+	}
+
+	// Download maps from online
+	public static void DownloadMapSources(){
+		// TODO
+	}
+
+	// Merge map sources into one file
+	public static void ConcatenateMapSources(){
+		// TODO
+	}
+
+	// make tiles from dataset
+	public static void MakeTiles(){
+		// TODO
+	}
+
+	// correlate school scores with the schools
+	public static void CorrelateScoresWithSchools(){
+		// TODO
+	}
+
+}
