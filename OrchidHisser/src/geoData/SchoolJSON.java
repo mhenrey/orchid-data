f517041eacb4d63a27b83928c3485331fe7d4c91
Mike Henrey <mike.henrey@gmail.com>
2016-10-23 10:03:21 -0700

    Factored out method for comparing score sources from main file to school file.

--- OrchidHisser/src/geoData/SchoolJSON.java
+++ OrchidHisser/src/geoData/SchoolJSON.java
@@ -3,13 +3,19 @@
  */
 package geoData;
 
-import java.io.File;
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
 import java.util.List;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 
-import settings.GeoSource;
-import settings.SchoolSettings;
+import orchidHisser.OrchidHisser;
 
 /**
  * @author mike
@@ -18,64 +24,158 @@ import settings.SchoolSettings;
 
 @JsonIgnoreProperties(ignoreUnknown = true)
 public class SchoolJSON {
-	private String name;
-	private String type;
+	private static final Logger logger = LogManager.getLogger(OrchidHisser.class);
 	private CRS crs;
 	private List<Feature> features;
+	private String name;
+
+	private String type;
+
 	/**
-	 * @return the name
+	 * 
 	 */
-	public String getName() {
-		return name;
+	public void convertCRS() {
+
 	}
-	/**
-	 * @param name the name to set
-	 */
-	public void setName(String name) {
-		this.name = name;
+
+	// correlate school scores with the schools
+	public void correlateScoresWithSchools(String csvFile) {
+		logger.trace("Correlating data.");
+
+		ArrayList<String[]> scoreDataEntries = ReadScoreListCSVFile(csvFile);
+
+		for (Feature elementarySchool : features) {
+			String schoolName = elementarySchool.getProperties().getSimplifiedName();
+
+			// set the rank to -1 (default, will indicate no data)
+			elementarySchool.getProperties().setRank(-1.0);
+
+			// search for a matching name
+			for (String[] entry : scoreDataEntries) {
+				String entryName = getSimplifiedName(entry[0]);
+				// if a match is found, set the rank
+				if (entryName.equalsIgnoreCase(schoolName)) {
+					double rank = Double.parseDouble(entry[2]);
+					elementarySchool.getProperties().setRank(rank);
+					break;
+				}
+			}
+		}
 	}
-	/**
-	 * @return the type
-	 */
-	public String getType() {
-		return type;
-	}
-	/**
-	 * @param type the type to set
-	 */
-	public void setType(String type) {
-		this.type = type;
-	}
+
 	/**
 	 * @return the crs
 	 */
 	public CRS getCrs() {
 		return crs;
 	}
-	/**
-	 * @param crs the crs to set
-	 */
-	public void setCrs(CRS crs) {
-		this.crs = crs;
-	}
+
 	/**
 	 * @return the features
 	 */
 	public List<Feature> getFeatures() {
 		return features;
 	}
+
 	/**
-	 * @param features the features to set
+	 * @return the name
+	 */
+	public String getName() {
+		return name;
+	}
+
+	// method to strip unnecessary characters from the school name
+	public String getSimplifiedName(String name) {
+		if (name.contains("Elem.")) {
+			return name.replace(" Elem.", "");
+		} else if (name.contains("Elementary")) {
+			return name.replace(" Elementary", "");
+		}
+		return name;
+	}
+
+	/**
+	 * @return the type
+	 */
+	public String getType() {
+		return type;
+	}
+
+	/**
+	 * @param csvFile
+	 * @return
+	 */
+
+	private ArrayList<String[]> ReadScoreListCSVFile(String csvFile) {
+		// from
+		// https://www.mkyong.com/java/how-to-read-and-parse-csv-file-in-java/
+		BufferedReader br = null;
+		String line = "";
+		String delimiter = ",";
+
+		ArrayList<String[]> scoreDataEntries = new ArrayList<String[]>();
+
+		try {
+
+			br = new BufferedReader(new FileReader(csvFile));
+
+			// get the first line containing the header
+			String[] header = br.readLine().split(delimiter);
+
+			// TODO validate header
+
+			// now read the remaining lines
+			while ((line = br.readLine()) != null) {
+				// and store in a list
+				scoreDataEntries.add(line.split(delimiter));
+			}
+
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		} finally {
+			if (br != null) {
+				try {
+					br.close();
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
+			}
+		}
+		return scoreDataEntries;
+	}
+
+	/**
+	 * @param crs
+	 *            the crs to set
+	 */
+	public void setCrs(CRS crs) {
+		this.crs = crs;
+	}
+
+	/**
+	 * @param features
+	 *            the features to set
 	 */
 	public void setFeatures(List<Feature> features) {
 		this.features = features;
 	}
+
 	/**
-	 * 
+	 * @param name
+	 *            the name to set
 	 */
-	public void convertCRS() {
-		
-		
+	public void setName(String name) {
+		this.name = name;
 	}
-	
+
+	/**
+	 * @param type
+	 *            the type to set
+	 */
+	public void setType(String type) {
+		this.type = type;
+	}
+
 }
--- OrchidHisser/src/orchidHisser/OrchidHisser.java
+++ OrchidHisser/src/orchidHisser/OrchidHisser.java
@@ -1,8 +1,7 @@
 package orchidHisser;
+
 import java.io.BufferedReader;
 import java.io.File;
-import java.io.FileNotFoundException;
-import java.io.FileReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
@@ -17,8 +16,6 @@ import settings.ScoreSource;
 
 //Import log4j classes.
 import org.apache.logging.log4j.Logger;
-import org.apache.logging.log4j.core.pattern.EqualsIgnoreCaseReplacementConverter;
-
 import com.fasterxml.jackson.core.JsonGenerationException;
 import com.fasterxml.jackson.core.JsonParseException;
 import com.fasterxml.jackson.databind.JsonMappingException;
@@ -36,19 +33,60 @@ public class OrchidHisser {
 	// Logger instance named "OrchidHisser".
 	private static final Logger logger = LogManager.getLogger(OrchidHisser.class);
 
-	public static void main(String[] args) throws JsonParseException, IOException {
-		logger.trace("Entering application.");
+	// Merge map sources into one file
+	public static SchoolJSON concatenateMapSources(MapSettings mapSettings) throws JsonParseException, IOException {
+		logger.trace("Concatenating map sources.");
 
-		boolean startupMode = false;
-		MapSettings mapSettings = LoadSettings(startupMode);
+		// for each source file, load it into classes
+		List<SchoolJSON> schoolJSONs = SchoolJSONFactory.FromSettings(mapSettings.getSchoolSettings());
 
-		downloadMapSources();
-		SchoolJSON concatenatedElementarySchoolJSON = concatenateMapSources(mapSettings);
-		concatenatedElementarySchoolJSON.convertCRS();
+		// create a new JSON collection that will contain information from all
+		// source files
+		SchoolJSON concatenatedElementaryJSON = new SchoolJSON();
 
-		correlateScoresWithSchools(concatenatedElementarySchoolJSON);
+		// populate the heading information from the first JSON file element
+		concatenatedElementaryJSON.setCrs(schoolJSONs.get(0).getCrs());
+		concatenatedElementaryJSON.setName("Elementary Schools");
+		concatenatedElementaryJSON.setType(schoolJSONs.get(0).getType());
 
-		makeTiles(concatenatedElementarySchoolJSON);
+		// populate the features list with all elements from all JSON sources
+		ArrayList<Feature> concatenatedFeatureList = new ArrayList<Feature>();
+		for (SchoolJSON schoolJSON : schoolJSONs) {
+			for (Feature feature : schoolJSON.getFeatures()) {
+				concatenatedFeatureList.add(feature);
+			}
+		}
+		concatenatedElementaryJSON.setFeatures(concatenatedFeatureList);
+
+		return concatenatedElementaryJSON;
+	}
+
+	// Download maps from online
+	public static void downloadMapSources() {
+		logger.trace("Downloading map sources.");
+	}
+
+	// from https://www.mkyong.com/java/how-to-execute-shell-command-from-java/
+	private static String executeCommand(String command) {
+
+		StringBuffer output = new StringBuffer();
+
+		Process p;
+		try {
+			p = Runtime.getRuntime().exec(command);
+			p.waitFor();
+			BufferedReader reader = new BufferedReader(new InputStreamReader(p.getInputStream()));
+
+			String line = "";
+			while ((line = reader.readLine()) != null) {
+				output.append(line + "\n");
+			}
+
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+
+		return output.toString();
 
 	}
 
@@ -93,158 +131,38 @@ public class OrchidHisser {
 		return mapSettings;
 	}
 
-	// Download maps from online
-	public static void downloadMapSources() {
-		logger.trace("Downloading map sources.");
-	}
+	public static void main(String[] args) throws JsonParseException, IOException {
+		logger.trace("Entering application.");
 
-	// Merge map sources into one file
-	public static SchoolJSON concatenateMapSources(MapSettings mapSettings) throws JsonParseException, IOException {
-		logger.trace("Concatenating map sources.");
+		boolean startupMode = false;
+		MapSettings mapSettings = LoadSettings(startupMode);
 
-		// for each source file, load it into classes
-		List<SchoolJSON> schoolJSONs = SchoolJSONFactory.FromSettings(mapSettings.getSchoolSettings());
+		downloadMapSources();
+		SchoolJSON concatenatedElementarySchoolJSON = concatenateMapSources(mapSettings);
+		concatenatedElementarySchoolJSON.convertCRS();
 
-		// create a new JSON collection that will contain information from all
-		// source files
-		SchoolJSON concatenatedElementaryJSON = new SchoolJSON();
+		String csvFile = "bin/ScoreSources/Fraser2015.csv";
+		concatenatedElementarySchoolJSON.correlateScoresWithSchools(csvFile);
 
-		// populate the heading information from the first JSON file element
-		concatenatedElementaryJSON.setCrs(schoolJSONs.get(0).getCrs());
-		concatenatedElementaryJSON.setName("Elementary Schools");
-		concatenatedElementaryJSON.setType(schoolJSONs.get(0).getType());
+		makeTiles(concatenatedElementarySchoolJSON);
 
-		// populate the features list with all elements from all JSON sources
-		ArrayList<Feature> concatenatedFeatureList = new ArrayList<Feature>();
-		for (SchoolJSON schoolJSON : schoolJSONs) {
-			for (Feature feature : schoolJSON.getFeatures()) {
-				concatenatedFeatureList.add(feature);
-			}
-		}
-		concatenatedElementaryJSON.setFeatures(concatenatedFeatureList);
-		
-		return concatenatedElementaryJSON;
 	}
 
 	// make tiles from dataset
-	public static void makeTiles(SchoolJSON concatenatedElementaryJSON) throws JsonGenerationException, JsonMappingException, IOException {
-		
+	public static void makeTiles(SchoolJSON concatenatedElementaryJSON)
+			throws JsonGenerationException, JsonMappingException, IOException {
+
 		logger.trace("Generating tiles. Writing JSON to file.");
 		// write the json to file
 		ObjectMapper mapper = new ObjectMapper();
 		mapper.writeValue(new File("bin/GeoSources/concatenated_elementary.json"), concatenatedElementaryJSON);
-		
+
 		logger.trace("Generating converted projection.");
 		executeCommand(
 				"/usr/local/Cellar/gdal/1.11.5/bin/ogr2ogr -t_srs WGS84 -f geoJSON bin/GeoSources/converted_elementary.json bin/GeoSources/concatenated_elementary.json");
 		logger.trace("Now making .mbtiles file.");
 		executeCommand(
 				"/usr/local/Cellar/tippecanoe/1.13.0/bin/tippecanoe -o bin/GeoSources/concatenated_elementary.mbtiles bin/GeoSources/converted_elementary.json -f");
-	}
-
-	// from https://www.mkyong.com/java/how-to-execute-shell-command-from-java/
-	private static String executeCommand(String command) {
-
-		StringBuffer output = new StringBuffer();
-
-		Process p;
-		try {
-			p = Runtime.getRuntime().exec(command);
-			p.waitFor();
-			BufferedReader reader = new BufferedReader(new InputStreamReader(p.getInputStream()));
-
-			String line = "";
-			while ((line = reader.readLine()) != null) {
-				output.append(line + "\n");
-			}
-
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
-
-		return output.toString();
-
-	}
-
-	// correlate school scores with the schools
-	public static void correlateScoresWithSchools(SchoolJSON concatenatedElementarySchoolJSON) {
-		logger.trace("Correlating data.");
-		String csvFile = "bin/ScoreSources/Fraser2015.csv";
-		ArrayList<String[]> scoreDataEntries = ReadScoreListCSVFile(csvFile);
-		
-		for(Feature elementarySchool:concatenatedElementarySchoolJSON.getFeatures()){
-			String schoolName = elementarySchool.getProperties().getSimplifiedName();
-
-			// set the rank to -1 (default, will indicate no data)
-			elementarySchool.getProperties().setRank(-1.0);
-			
-			// search for a matching name
-			for (String[] entry:scoreDataEntries){
-				String entryName = getSimplifiedName(entry[0]);
-				// if a match is found, set the rank
-				if (entryName.equalsIgnoreCase(schoolName)){
-					double rank = Double.parseDouble(entry[2]);
-					elementarySchool.getProperties().setRank(rank);
-					break;
-				}
-			}
-		}
-	}
-
-	// method to strip unnecessary characters from the school name
-    public static String getSimplifiedName(String name) {
-        if (name.contains("Elem.")){
-        	return name.replace(" Elem.", "");
-        }
-        else if (name.contains("Elementary")){
-        	return name.replace(" Elementary", "");
-        }
-        return name;
-      }
-    
-	/**
-	 * @param csvFile
-	 * @return
-	 */
-	
-	private static ArrayList<String[]> ReadScoreListCSVFile(String csvFile) {
-		// from
-		// https://www.mkyong.com/java/how-to-read-and-parse-csv-file-in-java/
-		BufferedReader br = null;
-		String line = "";
-		String delimiter = ",";
-
-		ArrayList<String[]> scoreDataEntries = new ArrayList<String[]>();
-
-		try {
-
-			br = new BufferedReader(new FileReader(csvFile));
-
-			// get the first line containing the header
-			String[] header = br.readLine().split(delimiter);
-
-			// TODO validate header
-
-			// now read the remaining lines
-			while ((line = br.readLine()) != null) {
-				// and store in a list
-				scoreDataEntries.add(line.split(delimiter));
-			}
-
-		} catch (FileNotFoundException e) {
-			e.printStackTrace();
-		} catch (IOException e) {
-			e.printStackTrace();
-		} finally {
-			if (br != null) {
-				try {
-					br.close();
-				} catch (IOException e) {
-					e.printStackTrace();
-				}
-			}
-		}
-		return scoreDataEntries;
 	}
 
 }
